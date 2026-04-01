e7841b5a9de4d1b6f45472cbc1612858d463cc9d
Gean Dioses <40503619+antoleod@users.noreply.github.com>
2026-04-01 17:02:28 +0200

    Extend local worklets shim to satisfy reanimated runtime on web

--- scripts/ensure-worklets-shim.js
+++ scripts/ensure-worklets-shim.js
@@ -18,5 +18,43 @@ fs.writeFileSync(
   pluginPath,
   "module.exports = function () { return { name: 'react-native-worklets-plugin-shim', visitor: {} }; };\n"
 );
-fs.writeFileSync(entryPath, 'module.exports = {};\n');
+fs.writeFileSync(
+  entryPath,
+  [
+    'const serializableMappingCache = new WeakMap();',
+    'const RuntimeKind = { ReactNative: 0, UI: 1 };',
+    'const id = (v) => v;',
+    'const isWorkletFunction = (fn) => typeof fn === "function";',
+    'const runOnJS = (fn) => (...args) => fn(...args);',
+    'const runOnUI = (fn) => (...args) => fn(...args);',
+    'const runOnRuntime = () => (fn) => (...args) => fn(...args);',
+    'const executeOnUIRuntimeSync = (fn) => fn();',
+    'const makeShareable = (v) => v;',
+    'const createSerializable = (v) => v;',
+    'const createWorkletRuntime = () => ({});',
+    'const callMicrotasks = () => {};',
+    'const WorkletsModule = {',
+    '  scheduleOnUI: (worklet) => { if (typeof worklet === "function") worklet(); },',
+    '};',
+    'module.exports = {',
+    '  RuntimeKind,',
+    '  serializableMappingCache,',
+    '  isWorkletFunction,',
+    '  runOnJS,',
+    '  runOnUI,',
+    '  runOnRuntime,',
+    '  executeOnUIRuntimeSync,',
+    '  makeShareable,',
+    '  createSerializable,',
+    '  createWorkletRuntime,',
+    '  callMicrotasks,',
+    '  WorkletsModule,',
+    '  default: {',
+    '    RuntimeKind, serializableMappingCache, isWorkletFunction, runOnJS, runOnUI, runOnRuntime,',
+    '    executeOnUIRuntimeSync, makeShareable, createSerializable, createWorkletRuntime, callMicrotasks, WorkletsModule,',
+    '  },',
+    '};',
+    '',
+  ].join('\n')
+);
 console.log('[postinstall] created local react-native-worklets plugin shim');
